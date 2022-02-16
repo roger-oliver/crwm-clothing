996dba469530639583908b38ab4bb58f26c13437
Roger Oliver <roger@v9y.nl>
2022-02-16 23:59:01 +0100

    merged branch 04-add-shop-page
    commit a2e7c211c8d445058c03a4120ec25b3a60a75114
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Wed Feb 16 23:58:10 2022 +0100
    
        added images, shop page, collection preview, collection item;

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CollectionItem from '../collection-item/colletion-item.component';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {
+          // these part will always be called when the object is rerendered.
+          // performance concern!!!
+          items
+            .filter((_, index) => index < 4)
+            .map(({id, ...otherCollectionItemProps}) => {
+            return (
+              <CollectionItem key={id} {...otherCollectionItemProps}/>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
