3efe74bf33d443c110bb95bfa50a4710e6334e49
Roger Oliver <roger@v9y.nl>
2022-02-20 15:46:55 +0100

    added custom button component; added custom form input added sign in component; added page to include the sign in and sign up components;

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({ email: '', password: '' });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2 className='title'>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form action="" onSubmit={this.handleSubmit}>
+          <FormInput
+            label='Email'
+            type="email"
+            name="email"
+            id="email"
+            value={this.state.email}
+            handleChange={this.handleInputChange}
+          />
+
+          <FormInput
+            label='Password'
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            handleChange={this.handleInputChange}
+          />
+
+          <CustomButton type="submit" >Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
