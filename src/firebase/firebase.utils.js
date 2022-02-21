e2432162b16d0df1caa967251bf0249ad49f4be2
Roger Oliver <roger@v9y.nl>
2022-02-22 00:11:32 +0100

    added sign up; added function to create a user profile; added some css;

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,6 +6,31 @@ const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+  const firestore = firebase.firestore();
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error when creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
+
 export const auth = firebase.auth();
 export const store = firebase.firestore();
 
