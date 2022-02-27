b77db1754d29c2beb26ce86e5f39ada14e145c52
Roger Oliver <roger@v9y.nl>
2022-02-27 14:13:52 +0100

    Merged in Branch 16-add-cart-icon-items-count
    commit 7d46c6f6a8254d1dabb036fc2a5571a96f9dda91
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Sun Feb 27 13:52:05 2022 +0100
    
        added unefficient way to update cart icon counter;

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,11 +6,11 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">100</span>
+      <span className="item-count">{ itemCount }</span>
     </div>
   )
 }
@@ -19,4 +19,10 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    itemCount: cartItems.reduce((count, cartItem) => count += cartItem.quantity, 0)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
