35e6d25032bc2d44f1cf2113d9fc32a554948a2e
Roger Oliver <roger@v9y.nl>
2022-02-27 23:21:59 +0100

    Merged in branch 19-finish-cart-dropdown
    commit f873af272f43e5bfdd4388762959e964f6a8a7dc
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Sun Feb 27 23:20:28 2022 +0100
    
        added message when no item was added;
        added router to checkout;
        added naked checkout component;

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
@@ -7,15 +8,19 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, history }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {
+          cartItems.length ?
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          )) :
+          <span className="empty-message">Your cart is empty</span>
+        }
       </div>
-      <CustomButton>Go To Checkout</CustomButton>
+      <CustomButton onClick={() => history.push('/checkout')} >Go To Checkout</CustomButton>
     </div>
   );
 };
@@ -24,4 +29,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
