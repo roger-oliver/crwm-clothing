24ba715b4710601f407fe0471caf57a839cf6bf7
Roger Oliver <roger@v9y.nl>
2022-02-28 00:14:09 +0100

    added checkout component and styles; added cart total selector; updated checkout component; added toggle cart dropdown to hide the cart dropdown;

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,20 +1,26 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
-const selectCart = state => state.cart;
+const selectCart = (state) => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCart],
-  cart => cart.cartItems
+  (cart) => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems => 
-    cartItems.reduce((count, cartItem) => count += cartItem.quantity, 0)
-
+  (cartItems) =>
+    cartItems.reduce((count, cartItem) => (count += cartItem.quantity), 0)
 );
 
 export const selectCartHidden = createSelector(
   [selectCart],
-  cart => cart.hidden
+  (cart) => cart.hidden
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity * cartItem.price,
+    0
+  )
 );
