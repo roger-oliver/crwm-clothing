14a108a1103a0d6f43b9b269dae89986d6d08895
Roger Oliver <roger@v9y.nl>
2022-02-28 00:24:33 +0100

    Merged in branch 20-finish-checkout-page
    commit 24ba715b4710601f407fe0471caf57a839cf6bf7
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Mon Feb 28 00:14:09 2022 +0100
    
        added checkout component and styles;
        added cart total selector;
        updated checkout component;
        added toggle cart dropdown to hide the cart dropdown;

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -20,7 +21,11 @@ const CartDropdown = ({ cartItems, history }) => {
           <span className="empty-message">Your cart is empty</span>
         }
       </div>
-      <CustomButton onClick={() => history.push('/checkout')} >Go To Checkout</CustomButton>
+      <CustomButton onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+        
+      }} >Go To Checkout</CustomButton>
     </div>
   );
 };
