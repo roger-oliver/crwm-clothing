7d2a1118dfb860e673d88f1603ec4da6bc126853
Roger Oliver <roger@v9y.nl>
2022-02-27 21:56:03 +0100

    Merged in Branch 18-add-user-selector
    commit dbf72a866fb028e30b2c83340b0462f3d7517e0e
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Sun Feb 27 21:54:41 2022 +0100
    
        added user selector to return current user;
        added selector to the cart hidden state;
        changed all points where we destructured state to get sliced parts of it;

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
@@ -19,8 +20,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = state => {
-  return { cartItems: selectCartItems(state) };
-};
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
+});
 
 export default connect(mapStateToProps)(CartDropdown);
