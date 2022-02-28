f21f781bda5672be131d9182d9fe3c7c677f4883
Roger Oliver <roger@v9y.nl>
2022-02-28 01:30:47 +0100

    added new action to remove item from cart;

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item,
 });
+
+export const clearItemFromCart = cartId => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: cartId,
+})
