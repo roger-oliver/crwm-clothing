061df5524330cb3f83aec32471059c68df7c7b4d
Roger Oliver <roger@v9y.nl>
2022-03-07 00:43:25 +0100

    Merged in branch 23-add-redux-persist-package
    commit 84396f52168f961239611c0d3c89766949966d74
    Author: Roger Oliver <roger@v9y.nl>
    Date:   Mon Mar 7 00:40:52 2022 +0100
    
        added redux-persist;
        added the config to make the store be saved in browser local-storage;

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
-import { combineReducers } from "redux";
-import cartReducer from "./cart/cart.reducer";
-import userReducer from "./user/user.reducer";
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+import cartReducer from './cart/cart.reducer';
+import userReducer from './user/user.reducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
