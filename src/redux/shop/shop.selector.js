1c9374a6b7c012b47d7f821519b2c898318bc229
Roger Oliver <roger@v9y.nl>
2022-03-14 00:49:56 +0100

    wip;

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAPPING = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-}
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -18,5 +10,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionIdUrlParm => 
   createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAPPING[collectionIdUrlParm])
+    collections => collections[collectionIdUrlParm]
   )
